46a98cdf179dbc685c19819d9f4830967907faf4
movtiv <mowrain@yandex.com>
2020-08-26 15:29:35 +0800

    Xz

--- extract.sh
+++ extract.sh
@@ -3,9 +3,23 @@
 #   Date : 2020-08-26 14:20:06 CST
 # Author : movtiv
 
-set -eux
+set -eu
 set -o pipefail
 
 DIR=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
 cd "$DIR"
 
+TARGET=input
+COLLECT=output
+mkdir -p "$COLLECT"
+
+for f in "$TARGET"/*.zip; do
+    filename="${f##*\/}"
+    name="${filename%.*}"
+    echo "$name"
+    unzip "$f" -d temp
+    for t in temp/*.torrent; do
+        mv -v "$t" "$COLLECT"/"$name".torrent
+    done
+    rm -fr temp/
+done
